608dc5ce30814ca427fdf9b441a4a114a06ab220
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2025-04-29 11:06:25 -0300

    Adds phosphor-react dependency and refactors layout components
    Introduces phosphor-react for improved icon usage.
    Refactors layout components to correct naming inconsistencies and removes obsolete header component.
    
    Cleans up routing structure for better readability.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
-import { Router } from "./router";
+import { Router } from "./Router";
 import { BrowserRouter } from "react-router-dom";
 
 export function App() {
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { History } from "./pages/History";
-import { DefatultLayout } from "./layouts/DefaultLayout";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefatultLayout />}>
+      <Route path="/" element={<DefaultLayout />}>
         <Route index element={<Home />} />
         <Route path="history" element={<History />} />
       </Route>
