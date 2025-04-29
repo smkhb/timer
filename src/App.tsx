6c4138f2e71184fc405cfd9f02497470c7db7579
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2025-04-29 09:49:29 -0300

    Adds theme support with styled-components
    Implements a default theme for the application using styled-components.
    
    Wraps the main application in a ThemeProvider to apply theme styles to button components.
    
    Introduces a new TypeScript definition for the default theme to enhance type safety.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 import { Button } from "./components/Button";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="success" />
       <Button variant="danger" />
-      <Button />
-    </>
+    </ThemeProvider>
   );
 }
